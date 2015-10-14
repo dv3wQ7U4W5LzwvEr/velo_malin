ad4fc5946fd4a55ba88b0269c49e313ba592bd09
Tof69 <christophe.cmr@gmail.com>
2015-10-14 11:06:24 +0200

    Ajout page acceuil IHM

---
+++ src/main/java/IHM/Fenetre.java
@@ -0,0 +1,70 @@
+package IHM;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+
+public class Fenetre {
+	public static void main(String[] args) { 
+		
+		   JFrame fenetre = new JFrame();
+		   JPanel pan = new JPanel();
+		   JTabbedPane onglets = new JTabbedPane();
+		   
+		   JMenuBar menuBar = new JMenuBar();                
+           JMenu menu1 = new JMenu("Programmer son itinéraire");
+           JMenu menu2 = new JMenu("Voir les résultats");
+           JMenu menu3 = new JMenu("Recherche de station");
+           JMenu menu4 = new JMenu("Alertes");           
+           
+		    menuBar.add(menu1);
+		    menuBar.add(menu2);
+		    menuBar.add(menu3);
+		    menuBar.add(menu4);
+		    
+		    JLabel label0 = new JLabel("Cliquer sur les boutons",JLabel.CENTER);
+		    
+		    
+		    //titre liste station
+		    JLabel label1 = new JLabel("Toutes les stations",JLabel.CENTER);
+		    
+		    JLabel liste_station = new JLabel();
+ 
+           onglets.setMinimumSize(new Dimension(200,100));
+           onglets.setBackground(Color.gray);
+		   
+           
+           JPanel panel0 = new JPanel();
+           JPanel panel1 = new JPanel();
+           JPanel panel2 = new JPanel();
+           JPanel panel3 = new JPanel();
+           JPanel panel4 = new JPanel();
+           
+           panel0.add(label0);
+           
+           onglets.addTab("Accueil", panel0);
+           onglets.addTab("Programmer son itinéraire", panel1);
+           onglets.addTab("Lancer une recherche", panel2);
+           onglets.addTab("Recherche de station", panel3);
+           onglets.addTab("Alertes", panel4);
+           
+           //onglets.setMnemonicAt(0, KeyEvent.VK_1);
+
+		    fenetre.setTitle("Velo Malin");
+		    fenetre.setSize(800, 400);
+		    fenetre.setLocationRelativeTo(null);
+		    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    
+		    	    
+		    
+		    
+		    //pan.add(menuBar, BorderLayout.NORTH);
+		    pan.setBackground(Color.RED);	    
+
+		    fenetre.add(pan); 
+		    fenetre.add(onglets);
+		    fenetre.setVisible(true);
+	 }
+
+}
