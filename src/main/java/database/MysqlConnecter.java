8d6984b639bd887965441fe126e9f92a19da5ba4
flauve <aflorianv@gmail.com>
2015-10-14 11:00:54 +0200

    maj du stockage dans la base de données

--- src/main/java/database/MysqlConnecter.java
+++ src/main/java/database/MysqlConnecter.java
@@ -4,6 +4,7 @@ package database;
 import model.Station;
 
 import java.sql.*;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,7 +21,7 @@ public class MysqlConnecter {
     private String user = "root";
     private String password = "root";
 
-    public void MysqlConnecter() {
+    public MysqlConnecter() {
         try {
             con = DriverManager.getConnection(url, user, password);
             st = con.createStatement();
@@ -33,7 +34,10 @@ public class MysqlConnecter {
             Logger lgr = Logger.getLogger(MysqlConnecter.class.getName());
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
             System.out.println("Erreur");
-        } finally {
+        }
+    }
+
+    public void disconnect() {
             try {
                 if (rs != null) {
                     rs.close();
@@ -48,35 +52,18 @@ public class MysqlConnecter {
             } catch (SQLException ex) {
                 Logger lgr = Logger.getLogger(MysqlConnecter.class.getName());
                 lgr.log(Level.WARNING, ex.getMessage(), ex);
-            } finally {
-                try {
-                    if (rs != null) {
-                        rs.close();
-                    }
-                    if (st != null) {
-                        st.close();
-                    }
-                    if (con != null) {
-                        con.close();
-                    }
-
-                } catch (SQLException ex) {
-                    Logger lgr = Logger.getLogger(MysqlConnecter.class.getName());
-                    lgr.log(Level.WARNING, ex.getMessage(), ex);
-                }
             }
-        }
-    }
-
-    public void connect() {
-        MysqlConnecter();
     }
 
     public void majStation(Station station) {
         try {
             st = con.createStatement();
+            Random rand = new Random();
+            int max = 99999;
+            int min = 1;
+            int nombreAleatoire = rand.nextInt(max - min + 1) + min;
             String request = "INSERT INTO STATIONS (id_station, nom, adresse, latitude, longitude, places) " +
-                     "values ( '', station.getNom(), station.getAdresse(), station.getLatitude(), station.getLongitude(), station.getPlaces())";
+                     "values ( " + nombreAleatoire + "," + station.getNom() + "," + station.getAdresse() + "," + station.getLatitude() + "," + station.getLongitude() + "," + station.getPlaces() + ")";
             rs = st.executeQuery(request);
         } catch (SQLException ex) {
             Logger lgr = Logger.getLogger(MysqlConnecter.class.getName());
--- src/main/java/was/model/SationDisponibiliteWas.java
+++ src/main/java/was/model/SationDisponibiliteWas.java
@@ -4,5 +4,5 @@ package was.model;
  * Created by flauve on 2015-10-14.
  */
 public class SationDisponibiliteWas {
-    
+
 }
