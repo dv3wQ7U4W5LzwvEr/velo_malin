a64f8f0b655ea763cdf0761b8bd30a6c8364d86d
Maxb018 <Maxb018@users.noreply.github.com>
2015-10-14 12:10:51 +0200

    Amelioration SQL Query Stations proximitées

--- src/main/java/recherche/StationsProximitees.java
+++ src/main/java/recherche/StationsProximitees.java
@@ -7,11 +7,21 @@ public class StationsProximitees {
 	
 	List getStationsProximité(int longitude, int latitude, int nb_stations){
 		
-		List listStations;
+		List listStations = new ArrayList();
 		
-		String sqlQuery = "SELECT id_station FROM Stations WHERE (latitude BETWEEN "+ latitude-0,0045 +" AND " + latitude+0,0045 +") AND (longitude BETWEEN "+ longitude-0,0045 +" AND "+ longitude+0,0045;
+		double distance = 0.0045,
+			latitudeMin= latitude-distance,
+			latitudeMax= latitude+distance,
+			longitudeMin= longitude-distance,
+			longitudeMax= longitude+distance;
+		
+		String sqlQuery = "SELECT id_station FROM Stations WHERE latitude > "+ latitudeMin +" AND latitude < "+ latitudeMax +" AND longitude > "+ longitudeMin +" AND longitude < "+ longitudeMax;
+		
+//		ou
+//		String sqlQuery = "SELECT id_station FROM Stations WHERE latitude BETWEEN "+ latitudeMin +" AND "+ latitudeMax +" AND longitude BETWEEN "+ longitudeMin +" AND "+ longitudeMax;
 		
 		return listStations;
+		
 		
 	}
 	
