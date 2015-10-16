ff2118dbfa9cb34359499c355b9281e028545a40
Tof69 <christophe.cmr@gmail.com>
2015-10-16 11:45:29 +0200

    Ajoute Classe Client.java

---
+++ src/main/java/model/Client.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Client {
+
+    private int id_client;
+
+	public int getId_client() {
+		return id_client;
+	}
+
+	public void setId_client(int id_client) {
+		this.id_client = id_client;
+	}
+
+ 
+    
+    
+}
