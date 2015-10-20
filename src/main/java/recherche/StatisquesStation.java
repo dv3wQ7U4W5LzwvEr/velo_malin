3516b83864d85c4e4188f6616f30cc9feebd2799
Maxb018 <Maxb018@users.noreply.github.com>
2015-10-20 18:41:48 +0200

    - Modification mineur sur StatisquesStation
    - Ajout dump BDD du 20/10/2015 17h
    - Ajout jar executable v0.1 (créé le 19/10/2015 à 20h40)

--- src/main/java/recherche/StatisquesStation.java
+++ src/main/java/recherche/StatisquesStation.java
@@ -1,9 +1,43 @@
 package recherche;
 
-import java.sql.ResultSet;
 import java.util.Date;
-import database.MysqlConnecter;
 
 public class StatisquesStation {
+
+	public int getMoyPlaceAllTimeOnStation(int id_station){
+		
+		String sqlQuery = "SELECT SUM(places_disponibles) FROM stationsdisponibilites WHERE id_station="+ id_station;
+
+//		int moyenne = resultat requete;
+	
+		return 0; // moyenne;
+		
+	}
+	
+	public int getMoyVeloAllTimeOnStation(int id_station){
+			
+			String sqlQuery = "SELECT SUM(places_occupees) FROM stationsdisponibilites WHERE id_station="+ id_station;
+			
+//			int moyenne = resultat requete;
+			
+			return 0; // moyenne;
+			
+	}
+	
+	public int getMoyVeloOnTimeOnStation(int id_station, Date jour){
+	    
+		long offSet = 60000*5;	//5 minites in millisecs
+		long longJour = jour.getTime();
+		
+		Date jourX = new Date(longJour + (10 * offSet));
+		Date jourY = new Date(longJour + (10 * offSet));
+		
+		String sqlQuery = "SELECT SUM(places_occupees) FROM stationsdisponibilites WHERE id_station="+ id_station +" AND jour BETWEEN "+ jourX +" AND "+ jourY;
+		
+//		int moyenne = resultat requete;
+		
+		return 0; // moyenne;
+		
+}
 	
 }
