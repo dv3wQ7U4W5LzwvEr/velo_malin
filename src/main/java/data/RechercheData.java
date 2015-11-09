c593ae5b523383d090412259599af56b9f557eda
flauve <aflorianv@gmail.com>
2015-11-09 11:31:56 +0100

    classe pour stocker les valeurs de recherches

---
+++ src/main/java/data/RechercheData.java
@@ -0,0 +1,66 @@
+package data;
+
+import java.util.Calendar;
+
+/**
+ * Created by laurel on 09/11/2015.
+ */
+public class RechercheData {
+
+    private static RechercheData instance = null;
+
+    private String departLat;
+    private String departLong;
+    private String arriveLat;
+    private String arriveLong;
+
+    private Calendar dateHeure;
+
+
+    protected static RechercheData getInstance() {
+        if (instance == null) {
+            instance = new RechercheData();
+        }
+        return instance;
+    }
+    
+    public String getDepartLat() {
+        return departLat;
+    }
+
+    public void setDepartLat(String departLat) {
+        this.departLat = departLat;
+    }
+
+    public String getDepartLong() {
+        return departLong;
+    }
+
+    public void setDepartLong(String departLong) {
+        this.departLong = departLong;
+    }
+
+    public String getArriveLat() {
+        return arriveLat;
+    }
+
+    public void setArriveLat(String arriveLat) {
+        this.arriveLat = arriveLat;
+    }
+
+    public String getArriveLong() {
+        return arriveLong;
+    }
+
+    public void setArriveLong(String arriveLong) {
+        this.arriveLong = arriveLong;
+    }
+
+    public Calendar getDateHeure() {
+        return dateHeure;
+    }
+
+    public void setDateHeure(Calendar dateHeure) {
+        this.dateHeure = dateHeure;
+    }
+}
---
+++ src/main/java/data/StatistiqueData.java
@@ -0,0 +1,7 @@
+package data;
+
+/**
+ * Created by laurel on 09/11/2015.
+ */
+public class StatistiqueData {
+}
